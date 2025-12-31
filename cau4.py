64a02fb40e9ba829d43e17856d9a3fa8ffca1d12
TrQNam <nam28082007@gmail.com>
2026-01-01 00:21:42 +0700

    viet code thu nghiem o nhanh moi

--- cau4.py
+++ cau4.py
@@ -3,7 +3,8 @@ def square(n):
 
 numbers = [1, 2, 3, 4]
 
-# map trả về một iterator, nên ta thường ép kiểu về list để xem kết quả
-result = map(square, numbers) 
+# map trả về một iterator , nên ta thường ép kiểu về list để xem kết quả
+result = map(str, numbers) 
 
-print(set(result)) # Kết quả: [1, 4, 9, 16]#
+print(list(result)) # Kết quả: [1, 4, 9, 16]
+print ('test branch nheee')
