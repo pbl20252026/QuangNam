b5edf3121ac76df41f450f3309fa3a6272a097bc
TrQNam <nam28082007@gmail.com>
2025-12-31 11:16:12 +0700

    first

--- cau4.py
+++ cau4.py
@@ -6,4 +6,4 @@ numbers = [1, 2, 3, 4]
 # map trả về một iterator, nên ta thường ép kiểu về list để xem kết quả
 result = map(square, numbers) 
 
-print(set(result)) # Kết quả: [1, 4, 9, 16]
+print(set(result)) # Kết quả: [1, 4, 9, 16]#
